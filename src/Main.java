cc8aceda2e188c429e61f1d89f310ab65fb95aa8
Sergey Smirnov <sergey.e.smirnov.94@gmail.com>
2023-10-01 14:50:31 +0300

    Calculator logic was added

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: "+ text.length());
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите первое число:");
+        int firstNumber = scan.nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = scan.nextInt();
+        int sum = firstNumber + secondNumber;
+        int diff = firstNumber - secondNumber;
+        int multiplication = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Сумма: "+ sum);
+        System.out.println("Разность: "+ diff);
+        System.out.println("Произведение: "+ multiplication);
+        System.out.println("Частное: "+ quotient);
     }
 }
